af56be7628d7e070f19550ed2cc5d2611f5484fd
Kwan Xin Jie <xjkwan1998@gmail.com>
2021-10-08 01:02:36 +0800

    Updates

---
+++ precache-manifest.9630104e5a32c8ffd281f2d8eebd60c6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78b8c24ca277339fd1de09bab3b62c5d",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "06777068fc312a3f2bf8",
+    "url": "/portfolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "8148de8c944560db5255",
+    "url": "/portfolio/static/css/main.db76a3b0.chunk.css"
+  },
+  {
+    "revision": "06777068fc312a3f2bf8",
+    "url": "/portfolio/static/js/2.b44a474d.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/portfolio/static/js/2.b44a474d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8148de8c944560db5255",
+    "url": "/portfolio/static/js/main.e723df9e.chunk.js"
+  },
+  {
+    "revision": "6c972d9418aa5b4e59ab",
+    "url": "/portfolio/static/js/runtime-main.1b893ad1.js"
+  }
+]);
